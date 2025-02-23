b6466a40fdc3440668e3cf29d00d0e6dc9aa55ff
steven <steven.zg@outlook.com>
2025-02-24 11:45:05 +1300

    fix: remove unused icon

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -6,7 +6,6 @@ import { usePathname } from "next/navigation"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
-  HomeIcon,
   BriefcaseIcon,
   FileTextIcon,
   UserIcon,
